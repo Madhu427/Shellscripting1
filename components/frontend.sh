fa7bad02b68e84048574ee51917a625b75b82c1f
Madhu427 <madhusudhanachary.k@gmail.com>
2021-12-22 17:49:37 +0530

    logs redirecting..

--- components/frontend.sh
+++ components/frontend.sh
@@ -31,12 +31,12 @@ STAT_CHECK() {
   fi
 }
 
-yum install nginx -y >>${LOG_FILE}
+yum install nginx -y
 
-STAT_CHECK $? "Nginx installation"
+STAT_CHECK $? "Nginx installation" &>>${LOG_FILE}
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-STAT_CHECK $? "Download Front-end" >>${LOG_FILE}
+STAT_CHECK $? "Download Front-end" &>>${LOG_FILE}
 
 cd /usr/share/nginx/html
 rm -rf *
