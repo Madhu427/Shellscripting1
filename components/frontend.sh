374c8ec6c079e78cf25abbeda30b9d97bed8032b
Madhu427 <madhusudhanachary.k@gmail.com>
2021-12-22 12:12:33 +0530

    to check rootuser -frontend

---
+++ components/frontend.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# yum install nginx -y
+# systemctl enable nginx
+# systemctl start nginx
+#Let's download the HTML content that serves the RoboSHop Project UI and deploy under the Nginx path.
+
+# curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+#Deploy in Nginx Default Location.
+
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-master static README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
+#Finally restart the service once to effect the changes.
+
+# systemctl restart nginx
+
+USER_UID=$(id -u)
+
+if [ ${USER_ID} -ne 0 ]; then
+  echo -e "\e[1;31m You should be root user to perform the script \e[0m"
+  exit
+fi
+
+yum install nginx -y
