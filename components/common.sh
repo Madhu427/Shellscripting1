6b6063edf87fbd815e7335c6251cc66d1b35bf26
Madhu427 <madhusudhanachary.k@gmail.com>
2021-12-24 17:46:14 +0530

    curl command -- common.sh

--- components/common.sh
+++ components/common.sh
@@ -1,7 +1,7 @@
 STAT_CHECK() {
 
 if [ $1 -ne 0 ]; then
-  echo -e "\e[1m${2} - /e[1;31mFailed\e[0m"
+  echo -e "\e[1m${2} - \e[1;31mFailed\e[0m"
   exit
 else
   echo -e "\e[1m${2} - \e[1;32mSuccess\e[0m"
