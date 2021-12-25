fe03e06c0de17799bd0f4a95825c55b6054b30b3
Madhu427 <madhusudhanachary.k@gmail.com>
2021-12-25 08:33:10 +0530

    O/P alligning in proper order

--- components/common.sh
+++ components/common.sh
@@ -1,8 +1,22 @@
 LOG_FILE=/tmp/roboshop.log
 rm -f ${LOG_FILE}
 
+MAX_LENGTH=$(cat components/databases.sh | grep STAT_CHECK | awk -F '"' '{print $2}' | awk {'print length'} | tail -n+15)
+
+if [ ${MAX_LENGTH} -lt 27 ]; then
+   MAX_LENGTH=27
+fi
+
 
 STAT_CHECK() {
+
+  SPACE=""
+  LENGTH=$(echo $2 | awk '{print length}')
+  LEFT=$((${MAX_LENGTH}-${LENGTH}))
+    while [ $LEFT -gt 0 ]; do
+      SPACE=$(echo -n "${SPACE} ")
+      LEFT=$((${LEFT}-1))
+    done
 
 if [ $1 -ne 0 ]; then
   echo -e "\e[1m${2} - \e[1;31mFailed\e[0m"
