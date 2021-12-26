6c85e99abee587ae0225cf418cd07ed8c848de3e
Madhu427 <madhusudhanachary.k@gmail.com>
2021-12-26 11:56:32 +0530

    user config --code dry --common.sh

--- components/catalouge.sh
+++ components/catalouge.sh
@@ -28,38 +28,4 @@
 
 source components/common.sh
 
-yum install nodejs make gcc-c++ -y &>>${LOG_FILE}
-STAT_CHECK $? "INSTALL Node JS"
-
-id roboshop &>>${LOG_FILE}
-if [ $? -ne 0 ]; then
-useradd roboshop &>>${LOG_FILE}
-STAT_CHECK $? "User added"
-fi
-
-DOWNLOAD catalogue
-
-rm -rf /home/roboshop/catalogue && mkdir -p /home/roboshop/catalogue && cp -r /tmp/catalogue-main/* /home/roboshop/catalogue
-&>>{LOG_FILE}
-STAT_CHECK $? "Copy catalogue content"
-
-cd /home/roboshop && npm install --unsafe-perm &>>{LOG_FILE}
-STAT_CHECK $? " Install node js dependencies"
-
-chown roboshop:roboshop -R /home/roboshop
-
-sed -i "s/MONGO_DNSNAME/mongo.devopsrobshop.com/" /home/roboshop/catalogue/systemd.service &>>${LOG_FILE} &&  mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-STAT_CHECK $? "update entries in systemd file"
-
-systemctl daemon-reload &>>{LOG_FILE} && systemctl start catalogue &>>{LOG_FILE} && systemctl enable catalogue &>>{LOG_FILE}
-STAT_CHECK $? "Start Catalogue Service"
-#curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
-#STAT_CHECK $? "Download catalouge"
-#
-#cd /home/roboshop && unzip -o /tmp/catalogue.zip &>>${LOG_FILE}
-#STAT_CHECK $? "moved catalogue to home"
-#
-#mv catalogue-main catalogue &>>${LOG_FILE} && sudo cd /home/roboshop/catalogue &>>${LOG_FILE}
-#
-#npm install &>>${LOG_FILE}
-#STAT_CHECK $? "Catalouge Installed"
+NODEJS catalogue
